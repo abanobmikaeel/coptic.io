43503ca98070ffd677cd3aee002cc224975a5228
abanobmikaeel <abanobmikaeel@gmail.com>
2026-06-23 20:43:05 -0400

    feat: adding speed insights

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from '@/components/Navbar'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import { NavigationProvider } from '@/contexts/NavigationContext'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { NextIntlClientProvider } from 'next-intl'
 import { getLocale, getMessages } from 'next-intl/server'
 
@@ -141,6 +142,7 @@ export default async function RootLayout({
 					</ThemeProvider>
 				</NextIntlClientProvider>
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	)
